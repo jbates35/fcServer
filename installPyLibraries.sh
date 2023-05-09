82f3684c7b607e71ce414206e8e4431205d24132
Jimmy Bates <cbates21@my.bcit.ca>
2023-05-09 00:09:45 -0700

    Added some libs

--- installPyLibraries.sh
+++ installPyLibraries.sh
@@ -3,3 +3,6 @@ sudo apt-get install libpq-dev
 python3 -m pip install psycopg2
 python3 -m pip install matplotlib
 python3 -m pip install apscheduler
+
+sudo apt-get install libxcb-xinerama0
+sudo apt install build-essential cmake libboost-all-dev
